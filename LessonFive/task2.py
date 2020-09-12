b6821ea72266388502b24d85b1391e93e95e77a6
ViktorDevRus <viktor.gulin@gmail.com>
2020-09-12 15:43:00 +0300

    changed task2.py + task3.py + task3_sample.txt

--- LessonFive/task2.py
+++ LessonFive/task2.py
@@ -3,10 +3,10 @@ try:
     with open(r'task2_sample.txt', 'r') as f_o:
         str_count = 0
         i = 1
-        words_count_list = {}
+        words_count_dict = {}
         for line in f_o:
             print(line, end='')
-            words_count_list[i] = len(line.split(" "))
+            words_count_dict[i] = len(line.split(" "))
             i += 1
             str_count += 1
 except FileNotFoundError:
@@ -18,5 +18,5 @@ except IOError:
     with open(r'task2_errors.txt', 'w') as log_file:
         print('An IOError (input / output) occurred while reading lines from a file.', file=log_file)
 print(f'\n\nКоличество строк в файле: {str_count}')
-for key, value in words_count_list.items():
+for key, value in words_count_dict.items():
     print(f'Количество слов в {key} строке: {value}')
---
+++ LessonFive/task3.py
@@ -0,0 +1,31 @@
+print('Получаем записи сотрудников из файла:\n')
+try:
+    with open(r'task3_sample.txt', 'r') as f_o:
+        staff_dict = {}
+        f_o.seek(16)
+        staff_count = 0
+        while True:
+            content = f_o.readline()
+            if content == 'end':
+                break
+            print(content, end='')
+            line_list = content.split(" ")
+            staff_dict[line_list[0][:-1]] = int(line_list[1][:-1])
+            staff_count += 1
+except FileNotFoundError:
+    print('Произошла ошибка "Файл не найден" на этапе чтения строк из файла.')
+    with open(r'task3_errors.txt', 'w') as log_file:
+        print('A FileNotFoundError (file not found) occurred while reading lines from a file.', file=log_file)
+except IOError:
+    print('Произошла ошибка ввода-вывода на этапе чтения строк из файла.')
+    with open(r'task3_errors.txt', 'w') as log_file:
+        print('An IOError (input / output) occurred while reading lines from a file.', file=log_file)
+# print(f'\n {staff_dict}')
+print('\nСотрудники с зарплатой меньше 20 000 рублей: ')
+for key, value in staff_dict.items():
+    if value < 20000:
+        print(key)
+sum_salary = 0
+for salary in staff_dict.values():
+    sum_salary += salary
+print(f'\n Средняя величина дохода сотрудников, рублей: {round(sum_salary/staff_count, 2)}')
